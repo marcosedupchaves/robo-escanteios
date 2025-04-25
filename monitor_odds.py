de17fff8899b244bb6d0c3a3d2565750ac2a15b0
marcosedupchaves <marcosedu_chaves@hotmail.com>
2025-04-24 21:23:15 -0300

    Update monitor_odds.py

--- monitor_odds.py
+++ monitor_odds.py
@@ -28,8 +28,6 @@ def get_odds():
     todos_jogos = jogos_ao_vivo + jogos_proximos
     odds_msg = "📊 *Odds de Gols e Escanteios:*\n\n"
 
-"
-
     for jogo in todos_jogos:
         fixture_id = jogo["fixture"]["id"]
         times = f"{jogo['teams']['home']['name']} x {jogo['teams']['away']['name']}"
